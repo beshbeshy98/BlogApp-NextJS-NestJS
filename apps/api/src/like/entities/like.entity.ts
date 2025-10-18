42701646dad418f7a3bcd78603ff0369023a6b47
Mohamed <mohamedelbeshbeshy98@gmail.com>
2025-10-19 01:23:46 +0200

    like resolver done

--- apps/api/src/like/entities/like.entity.ts
+++ apps/api/src/like/entities/like.entity.ts
@@ -1,7 +1,6 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
 import { Post } from 'src/post/entities/post.entity';
 import { User } from 'src/user/entities/user.entity';
-import { Tag } from 'src/tag/entities/tag.entity';
 import { CommentEntity } from 'src/comment/entities/comment.entity';
 
 @ObjectType()
@@ -15,15 +14,6 @@ export class Like {
 
     @Field(() => Post)
     post: Post;
-
-    @Field()
-    createdAt: Date;
-
-    @Field(() => User)
-    author: User
-
-    @Field(() => [Tag])
-    tags: Tag[];
 
     @Field(() => [CommentEntity])
     comments: CommentEntity[];
